57442aea2ae2cde274561002ab25d40ba654132f
Sampada-VS <samssv1@gmail.com>
2020-11-04 22:01:40 +0530

    Adding- created linked list by adding

--- src/main/java/com/bridgelabz/datastructure/LinkedList.java
+++ src/main/java/com/bridgelabz/datastructure/LinkedList.java
@@ -1,19 +1,35 @@
 package com.bridgelabz.datastructure;
 
 public class LinkedList {
-	static class MyNode <K>{
-		private K key;
-		private MyNode<K> next;
-		
-		public MyNode(K key) {
-			this.key=key;
-			this.next=null;
-		}
-		public MyNode getNext() {
-			return next;
-		}
-		public void setNext(MyNode next) {
-			this.next=next;
+	INode head;
+	INode tail;
+	
+	public LinkedList() {
+		this.head=null;
+		this.tail=null;
+	}
+	public void add (INode newNode) {
+		if (this.tail==null)
+			this.tail=newNode;
+		if(this.head==null)
+			this.head=newNode;
+		else {
+			INode tempNode=this.head;
+			this.head=newNode;
+			this.head.setNext(tempNode);
 		}
 	}
+	public void printMyNodes() {
+		StringBuffer nodes = new StringBuffer("All Nodes: ");
+		INode tempNode=head;
+		while (tempNode.getNext() != null) {
+			nodes.append(tempNode.getKey());
+			if (!tempNode.equals(tail))
+				nodes.append("->");
+			tempNode=tempNode.getNext();
+		}
+		nodes.append(tempNode.getKey());
+		System.out.println(nodes);
+	}
+	
 }
--- src/test/java/com/bridgelabz/datastructure/TestLinkedList.java
+++ src/test/java/com/bridgelabz/datastructure/TestLinkedList.java
@@ -1,13 +1,9 @@
 package com.bridgelabz.datastructure;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
-import com.bridgelabz.datastructure.LinkedList.MyNode;
-
 public class TestLinkedList {
-
 	@Test
 	public void givenThreeNumbers_WhenLinked_ShouldPassTest() {
 		MyNode<Integer> firstNode = new MyNode<>(56);
@@ -20,5 +16,20 @@ public class TestLinkedList {
 						secondNode.getNext().equals(thirdNode);
 		assertTrue(isLinked);
 	}
-
+	@Test
+	public void givenThreeNumbers_WhenAddedToLinked_ShouldAddToTop() {
+		MyNode<Integer> firstNode = new MyNode<>(70);
+		MyNode<Integer> secondNode = new MyNode<>(30);
+		MyNode<Integer> thirdNode = new MyNode<>(56);
+		LinkedList linkedlist= new LinkedList();
+		linkedlist.add(firstNode);
+		linkedlist.add(secondNode);
+		linkedlist.add(thirdNode);
+		linkedlist.printMyNodes();
+		boolean isAdded= linkedlist.head.equals(thirdNode) &&
+						linkedlist.head.getNext().equals(secondNode) &&
+						linkedlist.tail.equals(firstNode);
+		assertTrue(isAdded);
+	}
+	
 }
